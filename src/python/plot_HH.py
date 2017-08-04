bff1cc24090d233bbf90ba66873b3c7b42b169cb
Ross Johnstone <rhjohnstone@gmail.com>
2017-08-04 21:53:47 +0100

    3d scatter plot of CMA-ES best fits

--- src/python/plot_HH.py
+++ src/python/plot_HH.py
@@ -9,12 +9,14 @@ trace_name = "synthetic_HH"
 
 cmaes_best_fits_file, best_fit_png, best_fit_svg = ps.cmaes_and_figs_files(model_number, expt_name, trace_name)
 
-X, Y, Z, l = np.loadtxt(cmaes_best_fits_file).T
+all_points = np.loadtxt(cmaes_best_fits_file)
+
+data = all_points[:,:-1]
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-ax.scatter(X, Y, Z)
+ax.scatter(data.T)
 
 ax.set_title("Hodgkin Huxley 1952 CMA-ES best fits")
 ax.set_xlabel('$G_{Na}$')
