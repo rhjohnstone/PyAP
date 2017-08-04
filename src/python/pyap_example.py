40adbfb5610a7f11ef199845bf06517e1b8a3afa
Ross Johnstone <rhjohnstone@gmail.com>
2017-08-04 10:25:15 +0100

    Plot an AP from all available models in pyap_example

--- src/python/pyap_example.py
+++ src/python/pyap_example.py
@@ -16,33 +16,31 @@ def example_likelihood_function(trace):
 # 4. ten Tusscher
 # 5. O'Hara Rudy
 # 6. Davies (canine)
-# 7. Paci (SC-CM ventricular)
-# 8. Gokhale 2017 ex293
+# 7. Paci (SC-CM ventricular) (not available atm)
+# 8. Gokhale 2017 ex293 (not available atm)
 
-model_number = 4
 protocol = 1
 
 solve_start,solve_end,solve_timestep,stimulus_magnitude,stimulus_duration,stimulus_period,stimulus_start_time = ps.get_protocol_details(protocol)
-
-original_gs, g_parameters, model_name = ps.get_original_params(model_number)
-
 times = np.arange(solve_start,solve_end+solve_timestep,solve_timestep)
+for model_number in xrange(1,7):
+    original_gs, g_parameters, model_name = ps.get_original_params(model_number)
 
-ap = ap_simulator.APSimulator()
+    ap = ap_simulator.APSimulator()
+    ap.DefineStimulus(stimulus_magnitude,stimulus_duration,stimulus_period,stimulus_start_time)
+    ap.DefineSolveTimes(solve_start,solve_end,solve_timestep)
+    ap.DefineModel(model_number)
+    try:
+        true_trace = ap.SolveForVoltageTraceWithParams(original_gs)
+    except ap_simulator.CPPException as e:
+        print e.GetMessage
+        sys.exit()
 
-ap.DefineStimulus(stimulus_magnitude,stimulus_duration,stimulus_period,stimulus_start_time)
-ap.DefineSolveTimes(solve_start,solve_end,solve_timestep)
-ap.DefineModel(model_number)
-try:
-    true_trace = ap.SolveForVoltageTraceWithParams(original_gs)
-except ap_simulator.CPPException as e:
-    print e.GetMessage
-    sys.exit()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(times,true_trace)
+    ax.set_xlabel("Time (ms)")
+    ax.set_ylabel("Membrane voltage (mV)")
+    ax.set_title(model_name)
+    plt.show(block=True)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(times,true_trace)
-ax.set_xlabel("Time (ms)")
-ax.set_ylabel("Membrane voltage (mV)")
-ax.set_title(model_name)
-plt.show(block=True)
