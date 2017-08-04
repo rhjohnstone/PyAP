75cb343edfe0eb0157265e8abd9c30baae46f0c2
Ross Johnstone <rhjohnstone@gmail.com>
2017-08-04 22:06:56 +0100

    3d scatter plot of CMA-ES best fits

--- src/python/plot_HH.py
+++ src/python/plot_HH.py
@@ -2,6 +2,7 @@ import pyap_setup as ps
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.mlab import PCA
 
 model_number = 1
 expt_name = "synthetic_HH"
@@ -12,16 +13,9 @@ cmaes_best_fits_file, best_fit_png, best_fit_svg = ps.cmaes_and_figs_files(model
 all_points = np.loadtxt(cmaes_best_fits_file)
 
 data = all_points[:,:-1]
-data_mean = np.mean(data, axis=0)
 
-# Do an SVD on the mean-centered data.
-uu, dd, vv = np.linalg.svd(data - data_mean)
-
-p = np.polyfit(data[:,0], data[:,1:], deg=1)
-print p
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+pca = PCA(data)
+print pca
 
 ax.scatter(*data.T)
 
