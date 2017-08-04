0c728fa1619771ec4ab91ed30dec02c596d1f5a0
Ross Johnstone <rhjohnstone@gmail.com>
2017-08-04 15:32:38 +0100

    Option to not adapt proposal cov in MCMC

--- src/python/general_mcmc.py
+++ src/python/general_mcmc.py
@@ -13,6 +13,7 @@ requiredNamed = parser.add_argument_group('required arguments')
 requiredNamed.add_argument("--data-file", type=str, help="csv file from which to read in data", required=True)
 parser.add_argument("-i", "--iterations", type=int, help="total MCMC iterations", default=500000)
 parser.add_argument("--unscaled", action="store_true", help="perform MCMC sampling in unscaled 'conductance space'", default=False)
+parser.add_argument("--non-adaptive", action="store_true", help="do not adapt proposal covariance matrix", default=False)
 args, unknown = parser.parse_known_args()
 if len(sys.argv)==1:
     parser.print_help()
@@ -89,7 +90,7 @@ else:
     log_target = log_target_exp_scaled
 
 
-def do_mcmc(ap_model, expt_trace, temperature):#, theta0):
+def do_mcmc_adaptive(ap_model, expt_trace, temperature):#, theta0):
     #npr.seed(trace_number)
     print "Starting chain"
     start = time.time()
@@ -124,9 +125,7 @@ def do_mcmc(ap_model, expt_trace, temperature):#, theta0):
     chain[0, :] = np.concatenate((theta_cur, [log_target_cur]))
 
     loga = 0.
-    acceptance = 0.
-
-    
+    acceptance = 0.    
 
     status_when = total_iterations / 100
     adapt_when = 500*num_params
@@ -180,6 +179,95 @@ def do_mcmc(ap_model, expt_trace, temperature):#, theta0):
     chain[:,:-2] = original_gs**chain[:,:-2]  # return params scaled back into G-space
     return chain
 
+
+def do_mcmc_non_adaptive(ap_model, expt_trace, temperature):#, theta0):
+    #npr.seed(trace_number)
+    print "Starting chain"
+    start = time.time()
+    cmaes_best_fits_file, best_fit_png, best_fit_svg = ps.cmaes_and_figs_files(pyap_options["model_number"], expt_name, trace_name)
+    try:
+        cmaes_results = np.loadtxt(cmaes_best_fits_file)
+        ndim = cmaes_results.ndim
+        if ndim == 1:
+            best_gs = cmaes_results[:-1]
+        else:
+            best_index = np.argmin(cmaes_results[:,-1])
+            best_gs = cmaes_results[best_index,:-1]
+        initial_unscaled_gs = np.log(best_gs) / log_gs
+    except Exception, e:
+        print "\n",e,"\n"
+        initial_unscaled_gs = np.ones(num_params-1)
+    if args.unscaled:
+        theta_cur = np.concatenate((exponential_scaling(initial_unscaled_gs),[compute_initial_sigma(initial_unscaled_gs, ap_model, expt_trace)]))
+    else:
+        theta_cur = np.concatenate((initial_unscaled_gs,[compute_initial_sigma(initial_unscaled_gs, ap_model, expt_trace)]))
+    cov_estimate = 0.001*np.diag(np.abs(theta_cur))
+    print "\ntheta_cur:", theta_cur, "\n"
+    log_target_cur = log_target(theta_cur, ap_model, expt_trace)
+
+    total_iterations = args.iterations
+    thinning = 5
+    num_saved = total_iterations / thinning + 1
+    burn = num_saved / 4
+
+    chain = np.zeros((num_saved, num_params+1))
+    chain[0, :] = np.concatenate((theta_cur, [log_target_cur]))
+
+    loga = 0.
+    acceptance = 0.    
+
+    status_when = total_iterations / 100
+    adapt_when = 500*num_params
+
+    t = 1
+    s = 1
+    while t <= total_iterations:
+        theta_star = npr.multivariate_normal(theta_cur, np.exp(loga)*cov_estimate)
+        """try:
+            theta_star = npr.multivariate_normal(theta_cur, np.exp(loga)*cov_estimate)
+        except Warning as e:
+            print str(e)
+            print "Iteration:", t
+            print "temperature:", temperature
+            print "theta_cur:", theta_cur
+            print "loga:", loga
+            print "cov_estimate:", cov_estimate
+            sys.exit()"""
+        log_target_star = log_target(theta_star, ap_model, expt_trace)
+        u = npr.rand()
+        if np.log(u) < log_target_star - log_target_cur:
+            accepted = 1
+            theta_cur = theta_star
+            log_target_cur = log_target_star
+        else:
+            accepted = 0
+        acceptance = (t-1.)/t * acceptance + 1./t * accepted
+        if t % thinning == 0:
+            chain[t/thinning,:] = np.concatenate((theta_cur, [log_target_cur]))
+        if t % status_when == 0:
+            #pass
+            print t/status_when, "/", total_iterations/status_when
+            print "acceptance =", acceptance
+            time_taken_so_far = time.time()-start
+            estimated_time_left = int(total_iterations*time_taken_so_far/t - time_taken_so_far)
+            print "\n\nEstimated time remaining: {} s = {}-ish min\n\n".format(estimated_time_left, estimated_time_left/60)
+        if t > adapt_when:
+            gamma_s = 1./(s+1.)**0.6
+            loga += gamma_s*(accepted-0.25)
+            s += 1
+        t += 1
+    # discard burn-in before saving chain, just to save space mostly
+    time_taken = int(time.time() - start)
+    print "\n\nTime taken: {} s = {} min\n\n".format(time_taken,time_taken/60)
+    chain = chain[burn:, :]
+    chain[:,:-2] = original_gs**chain[:,:-2]  # return params scaled back into G-space
+    return chain
+
+
+if args.non_adaptive:
+    do_mcmc = do_mcmc_non_adaptive
+else:
+    do_mcmc = do_mcmc_adaptive
 
 protocol = 1
 solve_start, solve_end, solve_timestep, stimulus_magnitude, stimulus_duration, stimulus_period, stimulus_start_time = ps.get_protocol_details(protocol)
