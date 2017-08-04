7b9720807e6846ad92f0d345e5f907c31bee2817
Ross Johnstone <rhjohnstone@gmail.com>
2017-08-04 21:39:07 +0100

    3d scatter plot of CMA-ES best fits

---
+++ src/python/plot_HH.py
@@ -0,0 +1,19 @@
+import pyap_setup as ps
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+model_number = 1
+expt_name = "synthetic_HH"
+trace_name = "synthetic_HH"
+
+cmaes_best_fits_file, best_fit_png, best_fit_svg = ps.cmaes_and_figs_files(model_number, expt_name, trace_name)
+
+X, Y, Z, l = np.loadtxt(cmaes_best_fits_file).T
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(X, Y, Z)
+plt.show()
+
