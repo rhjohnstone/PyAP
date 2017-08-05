8cf8e82be0e5f85d5baaa46f1d23b390ac75b2b2
Ross Johnstone <rhjohnstone@gmail.com>
2017-08-05 16:09:40 +0100

    3d scatter plot of CMA-ES best fits

--- src/python/plot_HH.py
+++ src/python/plot_HH.py
@@ -20,16 +20,23 @@ eigenvectors, eigenvalues, V = np.linalg.svd(centred_data.T, full_matrices=False
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
-eigvend = mu + 0.5*eigenvectors[:,0]
-line = np.vstack((mu, eigvend))
-ax.plot(*line.T)
-
 ax.scatter(*data.T)
 ax.set_title("Hodgkin Huxley 1952 CMA-ES best fits")
 ax.set_xlabel('$G_{Na}$')
 ax.set_ylabel('$G_K$')
 ax.set_zlabel('$G_l$')
+xlim, ylim, zlim = ax.get_xlim(), ax.get_ylim(), ax.get_zlim()
+
+start = mu - eigenvectors[:,0]
+end = mu + eigenvectors[:,0]
+
+eigvend = mu + 0.5*eigenvectors[:,0]
+line = np.vstack((start, end))
+ax.plot(*line.T)
+
+ax.set_xlim(xlim)
+ax.set_ylim(ylim)
+ax.set_zlim(zlim)
 
 
 plt.show()
