97cdf68af2bf9a686b23d898eb3bc72bae55863c
Ross Johnstone <rhjohnstone@gmail.com>
2017-08-06 18:36:03 +0100

    Post-MCMC scatterplot matrix

--- src/python/general_plot_histograms.py
+++ src/python/general_plot_histograms.py
@@ -35,11 +35,13 @@ with open(options_file, 'r') as infile:
 original_gs, g_parameters, model_name = ps.get_original_params(pyap_options["model_number"])
 num_gs = len(original_gs)
 
+labels = g_parameters+[r"\sigma"]
+
 mcmc_file, log_file, png_dir = ps.mcmc_file_log_file_and_figs_dirs(pyap_options["model_number"], expt_name, trace_name, args.unscaled, args.non_adaptive)
 try:
     chain = np.loadtxt(mcmc_file)
 except:
-    sys.exit("\nCan't find (or load) {}\n".format(ps.trace_path))
+    sys.exit("\nCan't find (or load) {}\n".format(trace_path))
 
 for i in xrange(num_gs+1):
     fig = plt.figure()
@@ -66,5 +68,66 @@ ax.set_xlabel("Saved iteration")
 ax.set_ylabel('Log-target')
 fig.tight_layout()
 fig.savefig(png_dir+'log_target.png')
+plt.close()
+
+# plot scatterplot matrix of posterior(s)
+colormin, colormax = 1e9,0
+norm = matplotlib.colors.Normalize(vmin=5,vmax=10)
+hidden_labels = []
+count = 0
+# there's probably a better way to do this
+# I plot all the histograms to normalize the colours, in an attempt to give a better comparison between the pairwise plots
+while count < 2:
+    axes = {}
+    matrix_fig = plt.figure(figsize=(3*(num_gs+1),3*(num_gs+1)))
+    for i in range(num_params):
+        for j in range(i+1):
+            ij = str(i)+str(j)
+            subplot_position = num_params*i+j+1
+            if i==j:
+                axes[ij] = matrix_fig.add_subplot(num_params,num_params,subplot_position)
+                axes[ij].hist(chain[burn:,i],bins=50,normed=True,color='blue', edgecolor='blue')
+            elif j==0: # this column shares x-axis with top-left
+                axes[ij] = matrix_fig.add_subplot(num_params,num_params,subplot_position,sharex=axes["00"])
+                counts, xedges, yedges, Image = axes[ij].hist2d(chain[burn:,j],chain[burn:,i],cmap='hot_r',bins=50,norm=norm)
+                maxcounts = np.amax(counts)
+                if maxcounts > colormax:
+                    colormax = maxcounts
+                mincounts = np.amin(counts)
+                if mincounts < colormin:
+                    colormin = mincounts
+            else:
+                axes[ij] = matrix_fig.add_subplot(num_params,num_params,subplot_position,sharex=axes[str(j)+str(j)],sharey=axes[str(i)+"0"])
+                counts, xedges, yedges, Image = axes[ij].hist2d(chain[burn:,j],chain[burn:,i],cmap='hot_r',bins=50,norm=norm)
+                maxcounts = np.amax(counts)
+                if maxcounts > colormax:
+                    colormax = maxcounts
+                mincounts = np.amin(counts)
+                if mincounts < colormin:
+                    colormin = mincounts
+            axes[ij].xaxis.grid()
+            if (i!=j):
+                axes[ij].yaxis.grid()
+            if i!=num_params-1:
+                hidden_labels.append(axes[ij].get_xticklabels())
+            if j!=0:
+                hidden_labels.append(axes[ij].get_yticklabels())
+            if i==j==0:
+                hidden_labels.append(axes[ij].get_yticklabels())
+            if i==num_params-1:
+                axes[str(i)+str(j)].set_xlabel(labels[j])
+            if j==0 and i>0:
+                axes[str(i)+str(j)].set_ylabel(labels[i])
+                
+            plt.xticks(rotation=30)
+    norm = matplotlib.colors.Normalize(vmin=colormin,vmax=colormax)
+    count += 1
+
+    
+plt.setp(hidden_labels, visible=False)
+
+matrix_fig.tight_layout()
+matrix_fig.savefig(png_dir+'scatterplot_matrix.png')
+#matrix_fig.savefig(images_dir+"{}_{}_scatterplot_matrix.pdf".format(drug,channel))
 plt.close()
 
