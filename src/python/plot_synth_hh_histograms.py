e9f3d94d721e4b6cf9da8cf8765e88f3ed4158c0
Ross Johnstone <rhjohnstone@gmail.com>
2017-08-08 18:28:15 +0100

    Burn (1/, really) as optional argument

--- src/python/plot_synth_hh_histograms.py
+++ src/python/plot_synth_hh_histograms.py
@@ -14,6 +14,7 @@ requiredNamed.add_argument("--data-file", type=str, help="csv file from which to
 requiredNamed.add_argument("--seed", type=int, help="python random seed for initial position", required=True)
 parser.add_argument("--unscaled", action="store_true", help="perform MCMC sampling in unscaled 'conductance space'", default=False)
 parser.add_argument("--non-adaptive", action="store_true", help="do not adapt proposal covariance matrix", default=False)
+parser.add_argument("--burn", type=int, help="fraction of chain to discard as burn-in", default=0)
 args, unknown = parser.parse_known_args()
 if len(sys.argv)==1:
     parser.print_help()
@@ -51,8 +52,11 @@ except:
     sys.exit("\nCan't find (or load) {}\n".format(mcmc_file))
     
 saved_its, num_params_plus_1 = chain.shape
-#burn = saved_its/4
-burn = 0
+
+if args.burn:
+    burn = saved_its/args.burn
+else:
+    burn = 0
 
 for i in xrange(num_gs+1):
     fig = plt.figure()
