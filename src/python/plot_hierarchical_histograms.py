3fe0f0e59520d0ac4186a0218dba0140d250edd9
Ross Johnstone <rhjohnstone@gmail.com>
2017-08-25 12:42:39 +0100

    Parallel/series command line argument for plotting

--- src/python/plot_hierarchical_histograms.py
+++ src/python/plot_hierarchical_histograms.py
@@ -13,6 +13,11 @@ parser = argparse.ArgumentParser()
 requiredNamed = parser.add_argument_group('required arguments')
 requiredNamed.add_argument("--data-file", type=str, help="first csv file from which to read in data", required=True)
 requiredNamed.add_argument("--num-traces", type=int, help="number of traces to fit to, including the one specified as argument", required=True)
+
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-p', '--parallel', action='store_true', help="plot hMCMC that was run in parallel")
+group.add_argument('-s', '--series', action='store_true', help="plot hMCMC that was run in series")
+
 parser.add_argument("-nc", "--num-cores", type=int, help="number of cores to parallelise solving expt traces", default=1)
 #parser.add_argument("--unscaled", action="store_true", help="perform MCMC sampling in unscaled 'conductance space'", default=False)
 args, unknown = parser.parse_known_args()
@@ -25,6 +30,8 @@ split_trace_path = trace_path.split('/')
 expt_name = split_trace_path[4]
 trace_name = split_trace_path[-1][:-4]
 options_file = '/'.join( split_trace_path[:5] ) + "/PyAP_options.txt"
+
+
 
 pyap_options = {}
 with open(options_file, 'r') as infile:
@@ -42,12 +49,9 @@ data_clamp_off = pyap_options["data_clamp_off"]
 original_gs, g_parameters, model_name = ps.get_original_params(pyap_options["model_number"])
 num_gs = len(original_gs)
 
-if args.num_cores>1:
-    parallel = True
-else:
-    parallel = False
 
-mcmc_file, log_file, png_dir, pdf_dir = ps.hierarchical_mcmc_files(pyap_options["model_number"], expt_name, trace_name, args.num_traces, parallel)
+
+mcmc_file, log_file, png_dir, pdf_dir = ps.hierarchical_mcmc_files(pyap_options["model_number"], expt_name, trace_name, args.num_traces, args.parallel)
 
 chain = np.loadtxt(mcmc_file)
 num_saved_its, num_params = chain.shape
