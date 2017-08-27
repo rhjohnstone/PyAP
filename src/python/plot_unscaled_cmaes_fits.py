07005b67d6a8939e111629bdca686e6b879826d2
Ross Johnstone <rhjohnstone@gmail.com>
2017-08-27 20:49:47 +0100

    Plot unscaled best fits

--- src/python/plot_unscaled_cmaes_fits.py
+++ src/python/plot_unscaled_cmaes_fits.py
@@ -3,6 +3,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 
+
+def solve_for_voltage_trace(temp_g_params, _ap_model):
+    _ap_model.SetToModelInitialConditions()
+    try:
+        return _ap_model.SolveForVoltageTraceWithParams(temp_g_params)
+    except ap_simulator.CPPException, e:
+        print e.GetShortMessage
+        print "temp_g_params:\n", temp_g_params
+        print "original_gs:\n", original_gs
+        sys.exit()
+
+
 parser = argparse.ArgumentParser()
 requiredNamed = parser.add_argument_group('required arguments')
 requiredNamed.add_argument("--data-file", type=str, help="csv file from which to read in data", required=True)
@@ -17,6 +29,8 @@ expt_name = split_trace_path[4]
 trace_name = split_trace_path[-1][:-4]
 options_file = '/'.join( split_trace_path[:5] ) + "/PyAP_options.txt"
 
+expt_times, expt_trace = np.loadtxt(trace_path, delimiter=',').T
+
 pyap_options = {}
 with open(options_file, 'r') as infile:
     for line in infile:
@@ -27,7 +41,29 @@ with open(options_file, 'r') as infile:
             val = float(val)
         pyap_options[key] = val
 
+original_gs, g_parameters, model_name = ps.get_original_params(pyap_options["model_number"])
+num_gs = len(g_parameters)
+
+ap_model = ap_simulator.APSimulator()
+ap_model.DefineStimulus(0, 1, 1000, 0)  # no injected stimulus current
+ap_model.DefineModel(pyap_options["model_number"])
+ap_model.UseDataClamp(data_clamp_on, data_clamp_off)
+ap_model.SetExperimentalTraceAndTimesForDataClamp(expt_times, expt_trace)
+ap_model.DefineSolveTimes(expt_times[0], expt_times[-1], expt_times[1]-expt_times[0])
+ap_model.SetExtracellularPotassiumConc(pyap_options["extra_K_conc"])
+ap_model.SetIntracellularPotassiumConc(pyap_options["intra_K_conc"])
+ap_model.SetExtracellularSodiumConc(pyap_options["extra_Na_conc"])
+ap_model.SetIntracellularSodiumConc(pyap_options["intra_Na_conc"])
+ap_model.SetNumberOfSolves(pyap_options["num_solves"])
+
 cmaes_best_fits_file, best_fit_png, best_fit_svg = ps.cmaes_and_figs_files(pyap_options["model_number"], expt_name, trace_name, args.unscaled)
 
 fig, (ax1, ax2) = plt.subplots(1, sharey=True)
+best_fits = np.loadtxt(cmaes_best_fits_file)
+best_index = np.argmin(best_fits[:, -1])
+best_params = best_fits[best_index, :-1])
+best_trace = solve_for_voltage_trace(best_params, ap_model)
+ax1.plot(expt_times, expt_trace, color='red', label='Expt')
+ax1.plot(expt_times, best_trace, color='blue', label='Best fit')
+plt.show(block=True)
 
