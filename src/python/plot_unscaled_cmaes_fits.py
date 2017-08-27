b05941b0175d6b7fbebfd054b4bb7da0bc92d273
Ross Johnstone <rhjohnstone@gmail.com>
2017-08-27 21:12:57 +0100

    Two axes for plotting unscaled best fits

--- src/python/plot_unscaled_cmaes_fits.py
+++ src/python/plot_unscaled_cmaes_fits.py
@@ -16,16 +16,10 @@ def solve_for_voltage_trace(temp_g_params, _ap_model):
         print "original_gs:\n", original_gs
         sys.exit()
 
-
-parser = argparse.ArgumentParser()
-requiredNamed = parser.add_argument_group('required arguments')
-requiredNamed.add_argument("--data-file", type=str, help="csv file from which to read in data", required=True)
-parser.add_argument("--unscaled", action="store_true", help="perform MCMC sampling in unscaled 'conductance space'", default=False)
-args, unknown = parser.parse_known_args()
-if len(sys.argv)==1:
-    parser.print_help()
-    sys.exit(1)
-trace_path = args.data_file
+def data_file(t):
+    return data_file = "projects/PyAP/python/input/dog_teun/traces/dog_AP_trace_{}.csv".format(t)
+unscaled = True
+trace_path = data_file(150)
 split_trace_path = trace_path.split('/')
 expt_name = split_trace_path[4]
 trace_name = split_trace_path[-1][:-4]
@@ -60,14 +54,23 @@ ap_model.SetExtracellularSodiumConc(pyap_options["extra_Na_conc"])
 ap_model.SetIntracellularSodiumConc(pyap_options["intra_Na_conc"])
 ap_model.SetNumberOfSolves(pyap_options["num_solves"])
 
-cmaes_best_fits_file, best_fit_png, best_fit_svg = ps.cmaes_and_figs_files(pyap_options["model_number"], expt_name, trace_name, args.unscaled)
+
 
 fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-best_fits = np.loadtxt(cmaes_best_fits_file)
-best_index = np.argmin(best_fits[:, -1])
-best_params = best_fits[best_index, :-1]
-best_trace = solve_for_voltage_trace(best_params, ap_model)
-ax1.plot(expt_times, expt_trace, color='red', label='Expt')
-ax1.plot(expt_times, best_trace, color='blue', label='Best fit')
+ax1.set_ylabel('Membrane voltage (mV)')
+axes = [ax1, ax2]
+ts = ['dog_AP_trace_150', 'dog_AP_trace_151']
+for i, ax in enumerate(axes):
+    ax.grid()
+    ax.set_xlabel('Time (ms)')
+    cmaes_best_fits_file, best_fit_png, best_fit_svg = ps.cmaes_and_figs_files(pyap_options["model_number"], expt_name, ts[i], unscaled)
+    expt_times, expt_trace = np.loadtxt(data_file(150+i), delimiter=',').T
+    best_fits = np.loadtxt(cmaes_best_fits_file)
+    best_index = np.argmin(best_fits[:, -1])
+    best_params = best_fits[best_index, :-1]
+    best_trace = solve_for_voltage_trace(best_params, ap_model)
+    ax.plot(expt_times, expt_trace, color='red', label='Expt')
+    ax.plot(expt_times, best_trace, color='blue', label='Best fit')
+    ax.legend()
 plt.show(block=True)
 
