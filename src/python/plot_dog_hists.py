5cbbc22841c7813015dc54c3d74b6683d00594b4
Ross Johnstone <rhjohnstone@gmail.com>
2017-08-30 23:42:36 +0100

    Optional burn argument for plotting superimposed histograms

--- src/python/plot_dog_hists.py
+++ src/python/plot_dog_hists.py
@@ -11,6 +11,7 @@ requiredNamed.add_argument("--data-file", type=str, help="csv file from which to
 requiredNamed.add_argument("--num-traces", type=int, help="number of traces to plot hists of", required=True)
 parser.add_argument("--unscaled", action="store_true", help="perform MCMC sampling in unscaled 'conductance space'", default=True)
 parser.add_argument("--non-adaptive", action="store_true", help="do not adapt proposal covariance matrix", default=False)
+parser.add_argument("-b", "--burn", type=int, help="what fraction of samples to discard", default=4)
 args, unknown = parser.parse_known_args()
 if len(sys.argv)==1:
     parser.print_help()
@@ -42,7 +43,7 @@ for t in xrange(args.num_traces):
     mcmc_file, log_file, png_dir = ps.mcmc_file_log_file_and_figs_dirs(pyap_options["model_number"], expt_name, trace_name, True, False)
     chain = np.loadtxt(mcmc_file, usecols=range(15))
     saved_its, _ = chain.shape
-    burn = saved_its/4
+    burn = saved_its/args.burn
     for i in ids:
         axs[i].hist(chain[burn:, i], bins=40, color=mymap(color_idx[t]), alpha=0.2)
 
