21d7e5bf5eb92e96c87905f47aee345358d3c5d0
Ross Johnstone <rhjohnstone@gmail.com>
2017-09-28 15:37:57 +0100

    Plot hier. hists.

--- src/python/plot_hierarchical_histograms.py
+++ src/python/plot_hierarchical_histograms.py
@@ -15,10 +15,6 @@ requiredNamed = parser.add_argument_group('required arguments')
 requiredNamed.add_argument("--data-file", type=str, help="first csv file from which to read in data", required=True)
 requiredNamed.add_argument("--num-traces", type=int, help="number of traces to fit to, including the one specified as argument", required=True)
 
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('-p', '--parallel', action='store_true', help="plot hMCMC that was run in parallel")
-group.add_argument('-s', '--series', action='store_true', help="plot hMCMC that was run in series")
-
 parser.add_argument("-nc", "--num-cores", type=int, help="number of cores to parallelise solving expt traces", default=1)
 #parser.add_argument("--unscaled", action="store_true", help="perform MCMC sampling in unscaled 'conductance space'", default=False)
 args, unknown = parser.parse_known_args()
@@ -53,15 +49,14 @@ num_gs = len(original_gs)
 
 g_labels = ["${}$".format(g) for g in g_parameters]
 
-mcmc_file, log_file, png_dir, pdf_dir = ps.hierarchical_mcmc_files(pyap_options["model_number"], expt_name, trace_name, args.num_traces, args.parallel)
+parallel = True
 
-chain = np.loadtxt(mcmc_file)
-num_saved_its, num_params = chain.shape
-burn = num_saved_its/4
+
 
 trace_numbers = range(first_trace_number, first_trace_number+args.num_traces)
 
-for n, i in it.product(range(2), range(num_gs)):
+
+"""for n, i in it.product(range(2), range(num_gs)):
     print n, i
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -75,11 +70,13 @@ for n, i in it.product(range(2), range(num_gs)):
     ax.set_xlabel(xlabel)
     ax.set_ylabel("Normalised frequency")
     idx = (2+n)*num_gs + i
-    ax.hist(chain[burn:, idx], bins=40, color='blue', edgecolor='blue')
-    fig.savefig(png_dir+figname)
-    plt.close()
-
-for t, i in it.product(trace_numbers, range(num_gs)):
+    ax.hist(chain[burn:, idx], bins=40, color='blue', edgecolor='blue')"""
+    #fig.savefig(png_dir+figname)
+    #plt.close()
+    
+how_many_traces = [2,4]#,4,8]
+for j, N_e in enumerate(how_many_traces):
+    mcmc_file, log_file, png_dir, pdf_dir = ps.hierarchical_mcmc_files(pyap_options["model_number"], expt_name, trace_name, N_e, parallel)
     print t, i
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -88,8 +85,8 @@ for t, i in it.product(trace_numbers, range(num_gs)):
     ax.set_ylabel("Normalised frequency")
     idx = (2+t)*num_gs + i
     ax.hist(chain[burn:, idx], bins=40, color='blue', edgecolor='blue')
-    fig.savefig(png_dir+"trace_{}_{}.png".format(t, g_parameters[i]))
-    plt.close()
+    #fig.savefig(png_dir+"trace_{}_{}.png".format(t, g_parameters[i]))
+    #plt.close()
 
 print "sigma"
 fig = plt.figure()
@@ -98,7 +95,8 @@ ax.grid()
 ax.set_xlabel(r"\sigma")
 ax.set_ylabel("Normalised frequency")
 ax.hist(chain[burn:, -1], bins=40, color='blue', edgecolor='blue')
-fig.savefig(png_dir+"trace_{}_noise_sigma.png".format(n))
-plt.close()
+#fig.savefig(png_dir+"trace_{}_noise_sigma.png".format(n))
+#plt.close()
 
+plt.show(block=True)
 
