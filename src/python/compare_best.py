f6329c4c69be563a7390bc7383f5ded528a3642a
Ross Johnstone <rhjohnstone@gmail.com>
2017-10-02 15:52:40 +0100

    Compare best iteration with true params

---
+++ src/python/compare_best.py
@@ -0,0 +1,48 @@
+import ap_simulator  
+import pyap_setup as ps
+import argparse
+import numpy as np
+import sys
+import numpy.random as npr
+import time
+import multiprocessing as mp
+import argparse
+
+parser = argparse.ArgumentParser()
+requiredNamed = parser.add_argument_group('required arguments')
+requiredNamed.add_argument("--data-file", type=str, help="csv file from which to read in data", required=True)
+parser.add_argument("--unscaled", action="store_true", help="perform MCMC sampling in unscaled 'conductance space'", default=False)
+parser.add_argument("--non-adaptive", action="store_true", help="do not adapt proposal covariance matrix", default=False)
+args, unknown = parser.parse_known_args()
+if len(sys.argv)==1:
+    parser.print_help()
+    sys.exit(1)
+trace_path = args.data_file
+split_trace_path = trace_path.split('/')
+expt_name = split_trace_path[4]
+trace_name = split_trace_path[-1][:-4]
+options_file = '/'.join( split_trace_path[:5] ) + "/PyAP_options.txt"
+
+if args.seed:
+    npr.seed(args.seed)
+
+pyap_options = {}
+with open(options_file, 'r') as infile:
+    for line in infile:
+        (key, val) = line.split()
+        if (key == "model_number") or (key == "num_solves"):
+            val = int(val)
+        else:
+            val = float(val)
+        pyap_options[key] = val
+
+data_clamp_on = pyap_options["data_clamp_on"]
+data_clamp_off = pyap_options["data_clamp_off"]
+
+mcmc_file, log_file, png_dir = ps.mcmc_file_log_file_and_figs_dirs(pyap_options["model_number"], expt_name, trace_name, args.unscaled, args.non_adaptive)
+
+chain = np.loadtxt(mcmc_file)
+best_idx = np.argax(chain[:,-1])
+print "Best index:", best_idx
+best_params = chain[best_idx, :-1]
+print best_params
