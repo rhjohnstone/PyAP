13dfcea1ccab52f26432c320a1f5dc92422a4fee
Ross Johnstone <rhjohnstone@gmail.com>
2017-11-16 15:30:58 +0000

    Looking at fits to very noisy BR data

---
+++ src/python/analyse_synth_BR_noises.py
@@ -0,0 +1,16 @@
+import pyap_setup as ps
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = 0
+
+model_number = 2
+expt_name = "synth_BR_different_noises"
+trace_name = "synth_BR_different_noises_trace_{}.csv".format(t)
+unscaled = False
+cmaes_best_fits_file, best_fit_png, best_fit_svg = ps.cmaes_and_figs_files(model_number, expt_name, trace_name, unscaled)
+all_fits = np.loadtxt(cmaes_best_fits_file)
+best_fit_idx = np.argmin(all_fits[:, -1])
+best_params = all_fits[best_fit_idx, :-1]
+print best_params
+
