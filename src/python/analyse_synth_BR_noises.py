b33b5f8daf850cf5ab6a771f5522a29300993fdb
Ross Johnstone <rhjohnstone@gmail.com>
2017-11-16 15:33:11 +0000

    Fix cmaes output file name

--- src/python/analyse_synth_BR_noises.py
+++ src/python/analyse_synth_BR_noises.py
@@ -5,8 +5,12 @@ import numpy as np
 t = 0
 
 model_number = 2
+
+original_gs, g_parameters, model_name = ps.get_original_params(model_number)
+num_gs = len(original_gs)
+
 expt_name = "synth_BR_different_noises"
-trace_name = "synth_BR_different_noises_trace_{}.csv".format(t)
+trace_name = "synth_BR_different_noises_trace_{}".format(t)
 unscaled = False
 cmaes_best_fits_file, best_fit_png, best_fit_svg = ps.cmaes_and_figs_files(model_number, expt_name, trace_name, unscaled)
 all_fits = np.loadtxt(cmaes_best_fits_file)
