a5a43957410ed82a2476d5d157350b7e56fe3ae0
Ross Johnstone <rhjohnstone@gmail.com>
2017-11-16 15:36:44 +0000

    Do for all 32 traces

--- src/python/analyse_synth_BR_noises.py
+++ src/python/analyse_synth_BR_noises.py
@@ -2,7 +2,7 @@ import pyap_setup as ps
 import matplotlib.pyplot as plt
 import numpy as np
 
-t = 0
+
 
 model_number = 2
 
@@ -10,17 +10,20 @@ original_gs, g_parameters, model_name = ps.get_original_params(model_number)
 num_gs = len(original_gs)
 
 expt_name = "synth_BR_different_noises"
-trace_name = "synth_BR_different_noises_trace_{}".format(t)
 unscaled = False
-cmaes_best_fits_file, best_fit_png, best_fit_svg = ps.cmaes_and_figs_files(model_number, expt_name, trace_name, unscaled)
-all_fits = np.loadtxt(cmaes_best_fits_file)
-best_fit_idx = np.argmin(all_fits[:, -1])
-best_params = all_fits[best_fit_idx, :-1]
-print "best_params =", best_params
 
-diff_vector = best_params - original_gs
-print "diff_vector =", diff_vector
+for t in xrange(32)
+    print "Trace", t
+    trace_name = "synth_BR_different_noises_trace_{}".format(t)
+    cmaes_best_fits_file, best_fit_png, best_fit_svg = ps.cmaes_and_figs_files(model_number, expt_name, trace_name, unscaled)
+    all_fits = np.loadtxt(cmaes_best_fits_file)
+    best_fit_idx = np.argmin(all_fits[:, -1])
+    best_params = all_fits[best_fit_idx, :-1]
+    print "best_params =", best_params
 
-normalied_diff_vector = diff_vector/original_gs
-print "normalied_diff_vector =", normalied_diff_vector
+    diff_vector = best_params - original_gs
+    print "diff_vector =", diff_vector
+
+    normalied_diff_vector = diff_vector/original_gs
+    print "normalied_diff_vector =", normalied_diff_vector, "\n"
 
