9d2e67b14e352dff5865aa17e4c43e8584e0f537
Ross Johnstone <rhjohnstone@gmail.com>
2017-11-17 12:02:50 +0000

    Plot all marginals from hMCMC

--- src/python/plot_hierarchical_histograms.py
+++ src/python/plot_hierarchical_histograms.py
@@ -97,8 +97,21 @@ for i in xrange(num_gs):
         ax.axvline(expt_params[n, i], color='red')
     plt.xticks(rotation=30)
     fig.tight_layout()
-    #fig.savefig(png_dir+"{}_{}_traces_hierarchical_{}_marginal.png".format(expt_name, N_e, g_parameters[i]))
-    plt.show(block=True)
+    fig.savefig(png_dir+"{}_{}_traces_hierarchical_{}_marginal.png".format(expt_name, N_e, g_parameters[i]))
+    plt.close()
+
+for i in xrange(num_gs):
+    fig = plt.figure(figsize=(8,6))
+    ax = fig.add_subplot(111)
+    ax.grid()
+    ax.set_xlabel(r"$\sigma_{g_labels[i]}^2$")
+    ax.set_ylabel("Normalised frequency")
+    ax.hist(chain[:, num_gs+i], normed=True, bins=40, color='black', edgecolor='black')
+    ax.axvline((0.2*original_gs[i])**2, color='blue')  # just for synthetic OH atm
+    plt.xticks(rotation=30)
+    fig.tight_layout()
+    fig.savefig(png_dir+"{}_{}_traces_hierarchical_sigma2_{}_marginal.png".format(expt_name, N_e, g_parameters[i]))
+    plt.close()
 
 #plt.show(block=True)
 
