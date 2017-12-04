Fix normalising post pred

src/python/compare_sl_h_lnG.py
@@ -104,9 +104,10 @@ for i in xrange(num_gs):
             T = args.num_samples
             rand_idx = npr.randint(0, saved_its, T)
             m, s2 = h_chain[rand_idx, :].T
+            #print m, s2
             for t in xrange(T):
                 post_pred += norm.pdf(x, loc=m[t], scale=np.sqrt(s2[t]))
-        post_pred /= T
+        mle_pred /= T
         post_pred /= T
         axs[0].plot(x, mle_pred, lw=2, color=colour, label="$N_e = {}$".format(N_e))
         axs[1].plot(x, post_pred, lw=2, color=colour, label="$N_e = {}$".format(N_e))