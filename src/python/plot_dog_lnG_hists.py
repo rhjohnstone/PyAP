86bf2b9a99b4daeeeee954946eced467c2f7472d
Ross Johnstone <rhjohnstone@gmail.com>
2017-12-04 12:37:47 +0000

    Fix fn calls

--- src/python/plot_dog_lnG_hists.py
+++ src/python/plot_dog_lnG_hists.py
@@ -106,9 +106,9 @@ for n in xrange(N_e):
 
 num_ticks = 5
 for i in xrange(num_gs):
-    xlim = axs[i].get_xlim
+    xlim = axs[i].get_xlim()
     axs[i].set_xticks(np.round(np.linspace(xlim[0], xlim[1], num_ticks),2))
-    ylim = axs[i].get_ylim
+    ylim = axs[i].get_ylim()
     axs[i].set_yticks(np.round(np.linspace(ylim[0], ylim[1], num_ticks),2))
 
 plt.show()
