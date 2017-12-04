e116d2e30c7b56cbb48c39e95b5ba2dba2ff8bba
Ross Johnstone <rhjohnstone@gmail.com>
2017-12-04 16:40:58 +0000

    Plot so-called MLE predictive for all Ne

--- src/python/compare_sl_h_lnG.py
+++ src/python/compare_sl_h_lnG.py
@@ -55,9 +55,9 @@ parallel = True
 expt_params = np.loadtxt(expt_params_file)
 
 cs = ['#1b9e77','#d95f02','#7570b3']
-ax_titles = ["Single-level MLE", "Hierarchical post. pred."]
+ax_titles = ["Single-level MLE pred.", "Hierarchical post. pred."]
 num_pts = 201
-nums_expts = [2, 4, 8]#, 16, 32]
+nums_expts = [2, 4, 8, 16, 32]
 total_nums_expts = len(nums_expts)
 color_idx = np.linspace(0, 1, total_nums_expts)
 
@@ -120,7 +120,7 @@ for i in xrange(num_gs):
             
     fig.tight_layout()
     plt.show()
-    #fig.savefig(h_png_dir+'sl_mle_and_h_post_pred_{}.png'.format(g_parameters[i]))
+    fig.savefig(h_png_dir+'sl_mle_pred_and_h_post_pred_{}.png'.format(g_parameters[i]))
     plt.close()
 
         
