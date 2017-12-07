fb3ebdb9101d0eb62c8e176b84393b3490e79e26
Ross Johnstone <rhjohnstone@gmail.com>
2017-12-07 21:54:46 +0000

    Single legend outside axes

--- src/python/plot_lnG_posterior_predictives.py
+++ src/python/plot_lnG_posterior_predictives.py
@@ -142,7 +142,7 @@ for i in xrange(num_gs):
     
 print (true, prior)+lines
 print labels
-fig.legend((true, prior)+lines, labels, loc=9, mode="expand", ncol=2+len(nums_expts), borderaxespad=0.)
+fig.legend((true, prior)+lines, labels, loc=9, mode="expand", ncol=2+len(nums_expts), bbox_to_anchor=(0.5, 1.05))
 
 fig.tight_layout()
 
