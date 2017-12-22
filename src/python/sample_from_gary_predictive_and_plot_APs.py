af456d20e68524d96035b5a896b99393b564b4d8
Ross Johnstone <rhjohnstone@gmail.com>
2017-12-22 15:38:08 +0000

    Create file to sample from Gary-predictive

---
+++ src/python/sample_from_gary_predictive_and_plot_APs.py
@@ -0,0 +1,63 @@
+import pyap_setup as ps
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import argparse
+import numpy.random as npr
+from time import time
+import sys
+
+parser = argparse.ArgumentParser()
+requiredNamed = parser.add_argument_group('required arguments')
+requiredNamed.add_argument("--data-file", type=str, help="first csv file from which to read in data", required=True)
+parser.add_argument("-T", "--num-samples", type=int, help="number of AP samples to plot", default=0)
+parser.add_argument("-n", "--num-expts", type=int, help="number of traces to construct Gary-predictive from", required=True)
+#parser.add_argument("-x", "--num-pts", type=int, help="number of x points to plot Gary-predictive for", required=True)
+args, unknown = parser.parse_known_args()
+
+trace_path = args.data_file
+split_trace_path = trace_path.split('/')
+expt_name = split_trace_path[4]
+trace_name = split_trace_path[-1][:-4]
+options_file = '/'.join( split_trace_path[:5] ) + "/PyAP_options.txt"
+
+
+pyap_options = {}
+with open(options_file, 'r') as infile:
+    for line in infile:
+        (key, val) = line.split()
+        if (key == "model_number") or (key == "num_solves"):
+            val = int(val)
+        else:
+            val = float(val)
+        pyap_options[key] = val
+        
+data_clamp_on = pyap_options["data_clamp_on"]
+data_clamp_off = pyap_options["data_clamp_off"]
+
+
+split_trace_name = trace_name.split("_")
+
+        
+original_gs, g_parameters, model_name = ps.get_original_params(pyap_options["model_number"])
+num_gs = len(original_gs)
+
+g_labels = ["${}$".format(g) for g in g_parameters]
+
+m_true = np.log(original_gs)
+sigma2_true = 0.01
+
+N_e = args.num_expts
+
+gary_predictives = []
+
+for i in xrange(num_gs):
+    garyfile, garypng = ps.gary_predictive_file(expt_name, N_e, i)
+    gary_predictives.append(np.loadtxt(garyfile))
+    
+i = 0
+plt.plot(gary_predictives[i][:,0], gary_predictives[i][:,1])
+plt.show()
+
