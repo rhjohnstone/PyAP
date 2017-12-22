d7421b83ca84003e53350ad72da3a7b0b0748532
Ross Johnstone <rhjohnstone@gmail.com>
2017-12-22 11:59:49 +0000

    Option for x pts

--- src/python/compute_and_save_gary_predictives.py
+++ src/python/compute_and_save_gary_predictives.py
@@ -87,7 +87,8 @@ assert(np.all(chain_lengths==length))
 num_pts = args.num_pts
 xs = np.zeros((num_gs, num_pts))
 for i in xrange(num_gs):
-    xs[i, :] = np.linspace(0.9*mins[i], 1.1*maxs[i], num_pts)
+    xs[i, :] = np.linspace(mins[i]-0.1, maxs[i]+0.1, num_pts)
+
 
 
 T = args.num_samples
