694f4c196b898cefb8d3f0f0dd3904b192b1b53c
Ross Johnstone <rhjohnstone@gmail.com>
2017-12-23 12:17:10 +0000

    Solve and plot APs from Gary-predictive

--- src/python/sample_from_gary_predictive_and_plot_APs.py
+++ src/python/sample_from_gary_predictive_and_plot_APs.py
@@ -1,4 +1,5 @@
 import pyap_setup as ps
+import ap_simulator
 import numpy as np
 import matplotlib
 #matplotlib.use('Agg')
@@ -9,7 +10,14 @@ import numpy.random as npr
 from time import time
 import sys
 
-seed = 1
+
+def solve_for_voltage_trace_with_initial_V(temp_lnG_params, ap_model, expt_trace):
+    ap_model.SetToModelInitialConditions()
+    #ap_model.SetVoltage(expt_trace[0])
+    return ap_model.SolveForVoltageTraceWithParams(npexp(temp_lnG_params))
+    
+
+seed = 3
 npr.seed(seed)
 
 parser = argparse.ArgumentParser()
@@ -39,6 +47,11 @@ with open(options_file, 'r') as infile:
 data_clamp_on = pyap_options["data_clamp_on"]
 data_clamp_off = pyap_options["data_clamp_off"]
 
+try:
+    expt_times, expt_trace = np.loadtxt(trace_path,delimiter=',').T
+except:
+    sys.exit( "\n\nCan't find (or load) {}\n\n".format(trace_path) )
+
 
 split_trace_name = trace_name.split("_")
 
@@ -61,9 +74,9 @@ for i in xrange(num_gs):
 
 cs = ['#1b9e77','#d95f02','#7570b3']
 
-T = args.num_samples
-rand_samples = npr.rand(T)
-fig, axs = plt.subplots(1, 2, figsize=(7,3), sharey=True)
+
+
+"""fig, axs = plt.subplots(1, 2, figsize=(7,3), sharey=True)
 axs[0].set_ylabel("Cumulative dist.")
 p_s = [0, 11]
 for i, p in enumerate(p_s):
@@ -76,5 +89,37 @@ for i, p in enumerate(p_s):
         axs[i].plot([gary_predictives[p][0,0], gary_predictive_samples[t]], [rand_samples[t], rand_samples[t]], color=cs[1], lw=2)
         axs[i].plot([gary_predictive_samples[t], gary_predictive_samples[t]], [0, rand_samples[t]], color=cs[2], lw=2)
 fig.tight_layout()
+plt.show()"""
+
+
+
+ap_model = ap_simulator.APSimulator()
+if (data_clamp_on < data_clamp_off):
+    ap_model.DefineStimulus(0, 1, 1000, 0)  # no injected stimulus current
+    ap_model.DefineModel(cpp_model_number)
+    ap_model.UseDataClamp(data_clamp_on, data_clamp_off)
+    ap_model.SetExperimentalTraceAndTimesForDataClamp(expt_times, expt_trace)
+else:
+    ap_model.DefineStimulus(stimulus_magnitude, stimulus_duration, pyap_options["stimulus_period"], stimulus_start_time)
+    ap_model.DefineModel(cpp_model_number)
+ap_model.DefineSolveTimes(expt_times[0], expt_times[-1], expt_times[1]-expt_times[0])
+ap_model.SetExtracellularPotassiumConc(pyap_options["extra_K_conc"])
+ap_model.SetIntracellularPotassiumConc(pyap_options["intra_K_conc"])
+ap_model.SetExtracellularSodiumConc(pyap_options["extra_Na_conc"])
+ap_model.SetIntracellularSodiumConc(pyap_options["intra_Na_conc"])
+ap_model.SetNumberOfSolves(pyap_options["num_solves"])
+
+
+T = args.num_samples
+rand_samples = npr.rand(T)
+fig, ax = plt.subplots(1, 1, figsize=(4,3))
+ax.grid()
+ax.set_xlabel("Time (ms)")
+ax.set_ylabel("Membrane voltage (mV)")
+for t in xrange(T):
+    temp_lnGs = [np.interp(rand_samples[t], gary_predictives[p][:,1], gary_predictives[p][:,0]) for p in xrange(num_gs)]
+    ax.plot(expt_times, solve_for_voltage_trace_with_initial_V(temp_lnGs, ap_model, expt_trace), alpha=0.1, color='black')
+fig.tight_layout()
 plt.show()
+
 
