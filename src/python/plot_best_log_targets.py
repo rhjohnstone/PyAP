43ac5a55c1d4789bc2e71da83e9e778584991433
Ross Johnstone <rhjohnstone@gmail.com>
2017-12-23 22:58:40 +0000

    Plot best log-target found during MCMC

---
+++ src/python/plot_best_log_targets.py
@@ -0,0 +1,138 @@
+import pyap_setup as ps
+import ap_simulator
+import argparse
+import numpy as np
+import sys
+import numpy.random as npr
+import matplotlib
+#matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import itertools as it
+from scipy.stats import norm
+from scipy.stats import invgamma
+
+npexp = np.exp
+
+def solve_for_voltage_trace_with_initial_V(temp_lnG_params, ap_model, expt_trace):
+    ap_model.SetToModelInitialConditions()
+    ap_model.SetVoltage(expt_trace[0])
+    temp_Gs = npexp(temp_lnG_params)
+    solved = ap_model.SolveForVoltageTraceWithParams(temp_Gs)
+    #if solved[-1] > -70:
+    #    print temp_Gs
+    return solved
+
+
+#python_seed = 1
+#npr.seed(python_seed)
+
+randn = npr.randn
+sqrt = np.sqrt
+def sample_from_N_IG(eta):
+    mu, nu, alpha, beta = eta
+    sigma_squared_sample = invgamma.rvs(alpha,scale=beta)
+    sample = mu + sqrt(sigma_squared_sample/nu)*randn()
+    return sample, sigma_squared_sample
+
+parser = argparse.ArgumentParser()
+requiredNamed = parser.add_argument_group('required arguments')
+requiredNamed.add_argument("--data-file", type=str, help="first csv file from which to read in data", required=True)
+requiredNamed.add_argument("-n", "--num-traces", type=int, help="which hMCMC to use", required=True)
+
+args, unknown = parser.parse_known_args()
+if len(sys.argv)==1:
+    parser.print_help()
+    sys.exit(1)
+trace_path = args.data_file
+split_trace_path = trace_path.split('/')
+expt_name = split_trace_path[4]
+trace_name = split_trace_path[-1][:-4]
+options_file = '/'.join( split_trace_path[:5] ) + "/PyAP_options.txt"
+expt_params_file = '/'.join( split_trace_path[:5] ) + "/expt_params.txt"
+
+split_trace_name = trace_name.split("_")
+first_trace_number = int(split_trace_name[-1])
+
+pyap_options = {}
+with open(options_file, 'r') as infile:
+    for line in infile:
+        (key, val) = line.split()
+        if (key == "model_number") or (key == "num_solves"):
+            val = int(val)
+        else:
+            val = float(val)
+        pyap_options[key] = val
+        
+data_clamp_on = pyap_options["data_clamp_on"]
+data_clamp_off = pyap_options["data_clamp_off"]
+        
+original_gs, g_parameters, model_name = ps.get_original_params(pyap_options["model_number"])
+num_gs = len(original_gs)
+
+g_labels = ["${}$".format(g) for g in g_parameters]
+
+    
+N_e = args.num_traces
+
+color_idx = np.linspace(0, 1, N_e)
+
+
+m_true = np.log(original_gs)
+sigma2_true = 0.01
+
+sigma_lower = 1e-3
+sigma_upper = 25
+
+sigma_const = 1./(sigma_upper-sigma_lower)
+
+mu = m_true
+alpha = 4.*np.ones(num_gs)
+beta = (alpha+1.) * 0.04
+nu = 4.*beta / ((alpha+1.) * np.log(10)**2)
+
+prior_means = np.log(original_gs)
+prior_sd = 0.5*np.log(10)
+
+old_eta_js = np.vstack((mu, nu, alpha, beta)).T
+
+expt_traces = []
+MPDs = np.zeros((N_e, num_gs))
+for n in xrange(N_e):
+    temp_trace_number = first_trace_number + n
+    temp_trace_name = "_".join(split_trace_name[:-1])+"_"+str(temp_trace_number)
+    print temp_trace_name
+    sl_mcmc_file, sl_log_file, sl_png_dir = ps.mcmc_lnG_file_log_file_and_figs_dirs(pyap_options["model_number"], expt_name, temp_trace_name)
+    sl_chain = np.loadtxt(sl_mcmc_file)
+    max_target_idx = np.argmax(sl_chain[:,-1])
+    MPDs[n, :] = sl_chain[max_target_idx, :-2]
+    
+    plot_trace_number = 100 + n
+    plot_trace_path = "projects/PyAP/python/input/roche_ten_tusscher/traces/Trace_2_2_{}_1.csv".format(plot_trace_number)
+    expt_times, expt_trace = np.loadtxt(plot_trace_path, delimiter=',').T
+    expt_traces.append(expt_trace)
+
+ap_model = ap_simulator.APSimulator()
+if (data_clamp_on < data_clamp_off):
+    ap_model.DefineStimulus(0, 1, 1000, 0)  # no injected stimulus current
+    ap_model.DefineModel(pyap_options["model_number"])
+    ap_model.UseDataClamp(data_clamp_on, data_clamp_off)
+    ap_model.SetExperimentalTraceAndTimesForDataClamp(expt_times, expt_trace)
+else:
+    ap_model.DefineStimulus(stimulus_magnitude, stimulus_duration, pyap_options["stimulus_period"], stimulus_start_time)
+    ap_model.DefineModel(pyap_options["model_number"])
+ap_model.DefineSolveTimes(expt_times[0], expt_times[-1], expt_times[1]-expt_times[0])
+ap_model.SetExtracellularPotassiumConc(pyap_options["extra_K_conc"])
+ap_model.SetIntracellularPotassiumConc(pyap_options["intra_K_conc"])
+ap_model.SetExtracellularSodiumConc(pyap_options["extra_Na_conc"])
+ap_model.SetIntracellularSodiumConc(pyap_options["intra_Na_conc"])
+ap_model.SetNumberOfSolves(pyap_options["num_solves"])
+
+fig, ax = plt.subplots(1, 1)
+ax.plot(expt_times, expt_traces[0], color='blue')
+plt.show()
+
+
+
+
+
+
