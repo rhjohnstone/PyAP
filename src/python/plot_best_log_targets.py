83cf20a5c7f84866390d2fee71671f80b333fe9c
Ross Johnstone <rhjohnstone@gmail.com>
2017-12-23 23:06:04 +0000

    Plot best log-target found during MCMC

--- src/python/plot_best_log_targets.py
+++ src/python/plot_best_log_targets.py
@@ -138,6 +138,7 @@ ap_model.SetNumberOfSolves(pyap_options["num_solves"])
 
 fig, ax = plt.subplots(1, 1)
 ax.plot(expt_times, expt_traces[0], color='blue')
+ax.plot(expt_times, solve_for_voltage_trace_with_initial_V(MPDs[0, :], ap_model, expt_traces[0]), color='red')
 plt.show()
 
 
