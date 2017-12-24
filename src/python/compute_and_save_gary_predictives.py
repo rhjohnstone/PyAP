e9ed78d5ce5867065339a5fb1a39a65fae13ab7e
Ross Johnstone <rhjohnstone@gmail.com>
2017-12-24 15:47:40 +0000

    Reset xlims after fixing burnin

--- src/python/compute_and_save_gary_predictives.py
+++ src/python/compute_and_save_gary_predictives.py
@@ -92,29 +92,29 @@ xs = np.zeros((num_gs, num_pts))
 for i in xrange(num_gs):
     if pyap_options["model_number"]==4:
         #xs[i, :] = np.linspace(mins[i]-1, maxs[i]+1, num_pts)
-        if i==0:#
-            xs[i, :] = np.linspace(mins[i], maxs[i]+1, num_pts)
+        if i==0:
+            xs[i, :] = np.linspace(mins[i]+1, maxs[i]+0.5, num_pts)
         elif i==1:
             xs[i, :] = np.linspace(mins[i]-1, maxs[i]+0.5, num_pts)
         elif i==2:
             xs[i, :] = np.linspace(mins[i]-1, maxs[i]+1, num_pts)
-        elif i==3:#
+        elif i==3:
             xs[i, :] = np.linspace(mins[i]-3, maxs[i]+3, num_pts)
-        elif i==4:#
+        elif i==4:
             xs[i, :] = np.linspace(mins[i], maxs[i]+2, num_pts)
-        elif i==5:#
+        elif i==5:
             xs[i, :] = np.linspace(mins[i]-4, maxs[i]+9, num_pts)
-        elif i==6:#
+        elif i==6:
             xs[i, :] = np.linspace(mins[i]-1, maxs[i]+0.5, num_pts)
-        elif i==7:#
+        elif i==7:
             xs[i, :] = np.linspace(mins[i]-1, maxs[i]+1, num_pts)
-        elif i==8:#
+        elif i==8:
             xs[i, :] = np.linspace(mins[i]-4, maxs[i]+6, num_pts)
-        elif i==9:#
-            xs[i, :] = np.linspace(mins[i]-4, maxs[i]+4, num_pts)
-        elif i==10:#
+        elif i==9:
+            xs[i, :] = np.linspace(mins[i]-4, maxs[i]+5, num_pts)
+        elif i==10:
             xs[i, :] = np.linspace(mins[i]-1, maxs[i]+1, num_pts)
-        elif i==11:#
+        elif i==11:
             xs[i, :] = np.linspace(mins[i]-0.5, maxs[i]+0.5, num_pts)
         else:
             xs[i, :] = np.linspace(mins[i]-1, maxs[i]+1, num_pts)
