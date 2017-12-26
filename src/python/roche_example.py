e8f245ca50edabe87f81d7e592ce56e9e82661c9
Ross Johnstone <rhjohnstone@gmail.com>
2017-12-26 12:14:55 +0000

    Testing stim according to Roche protocol

--- src/python/roche_example.py
+++ src/python/roche_example.py
@@ -29,73 +29,39 @@ protocol = 1
 
 roche_file = "projects/PyAP/python/input/roche_ten_tusscher/traces/Trace_2_2_100_1.csv"
 expt_times, expt_trace = np.loadtxt(roche_file, delimiter=',').T
-plt.plot(expt_times, expt_trace)
-plt.show()
-sys.exit()
-
 
 solve_start,solve_end,solve_timestep,stimulus_magnitude,stimulus_duration,stimulus_period,stimulus_start_time = ps.get_protocol_details(protocol)
-for model_number in xrange(3,4):
 
-    solve_end = 400
-    times = np.arange(solve_start,solve_end+solve_timestep,solve_timestep)
+solve_start = expt_times[0]
+solve_end = expt_times[-1]
+solve_timestep = expt_times[1]-expt_times[0]
+stimulus_magnitude = -50  # wrong
+stimulus_duration = 2
+stimulus_period = 1000
+stimulus_start_time = 50
 
-    original_gs, g_parameters, model_name = ps.get_original_params(model_number)
-    original_gs = np.array(original_gs)
+model_number = 4
 
-    ap = ap_simulator.APSimulator()
-    ap.DefineStimulus(stimulus_magnitude,stimulus_duration,stimulus_period,stimulus_start_time)
-    ap.DefineSolveTimes(solve_start,solve_end,solve_timestep)
-    ap.DefineModel(model_number)
-    
-    ap.SetToModelInitialConditions()
-    expt_trace = ap.SolveForVoltageTraceWithParams(original_gs)
-    print "expt_trace original_gs done"
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.grid()
-    ax.plot(times, expt_trace)
-    ax.set_title(model_name)
-    plt.show(block=True)
-    sys.exit()
-    
-    p_size = 0.05
-    perturbations = [p_size*np.ones(len(original_gs)), -p_size*np.ones(len(original_gs))]
-    sosses = []
-    how_many_perturbs = 10
-    while len(perturbations) < how_many_perturbs:
-        temp_p = p_size*(npr.randint(0,2,len(original_gs))*2.-1.)
-        if not any((temp_p == x).all() for x in perturbations):
-            perturbations.append(temp_p)
-    print perturbations
-    
-    
-    #ax.grid()
-    
-    for p in perturbations:
-        scaled_gs = (1.+p) * original_gs
-        try:
-            ap.SetToModelInitialConditions()
-            trace = ap.SolveForVoltageTraceWithParams(scaled_gs)
-        except ap_simulator.CPPException as e:
-            print e.GetMessage
-            sys.exit()
-        print "\n", (1.+p)
-        ss = sos(trace)
-        print ss
-        sosses.append(ss)
-        #ax.plot(times, trace, label=m)
-    #ax.set_ylabel("Membrane voltage (mV)")
-    #axs.set_xlabel("Time (ms)")
-    
-    #ax.set_title(model_name)
-    #ax.legend()
-    
-    print sosses
-    
-    print "Min sos: {} from {}\n".format(np.min(sosses), perturbations[np.argmin(sosses)])
-    
-    #fig.tight_layout()
-    #plt.show(block=True)
+original_gs, g_parameters, model_name = ps.get_original_params(model_number)
+original_gs = np.array(original_gs)
+
+ap = ap_simulator.APSimulator()
+ap.DefineStimulus(stimulus_magnitude, stimulus_duration, stimulus_period, stimulus_start_time)
+ap.DefineSolveTimes(solve_start,solve_end,solve_timestep)
+ap.DefineModel(model_number)
+
+ap.SetToModelInitialConditions()
+test_trace = ap.SolveForVoltageTraceWithParams(original_gs)
+print "expt_trace original_gs done"
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.grid()
+ax.plot(expt_times, expt_trace)
+ax.plot(expt_times, test_trace)
+ax.set_title(model_name)
+plt.show(block=True)
+
+
+
 
