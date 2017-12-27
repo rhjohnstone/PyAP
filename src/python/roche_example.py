7128c9d21b3ab9baadd584a460c8637c6621b2a1
Ross Johnstone <rhjohnstone@gmail.com>
2017-12-27 16:57:38 +0000

    Rescaling caps

--- src/python/roche_example.py
+++ src/python/roche_example.py
@@ -116,7 +116,7 @@ for i, model_number in enumerate([3,4,5,7]):
     
     if model_number==3:
         stimulus_magnitude = -stim_amp * 1e-6
-        scale = 15
+        scale = 1e-6
         cap = true_cap * 1e-6
     elif model_number==4:  # not yet done
         scale = 1
