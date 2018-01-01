7aa61ae9f94e96784647151728b7d75f9c6ae26d
Ross Johnstone <rhjohnstone@gmail.com>
2018-01-01 19:34:51 +0000

    Gary-predictives for Roche with correct stim

--- src/python/compute_and_save_gary_predictives.py
+++ src/python/compute_and_save_gary_predictives.py
@@ -104,7 +104,7 @@ for i in xrange(num_gs):
         else:
             xs[i, :] = np.linspace(mins[i]-1.5, maxs[i]+1.5, num_pts)
     elif expt_name=="roche_paci_correct_units":
-        if i==0 or i==2 or i==3:
+        if i==0 or i==3:
             xs[i, :] = np.linspace(mins[i]-0.5, maxs[i]+0.5, num_pts)
         else:
             xs[i, :] = np.linspace(mins[i]-4., maxs[i]+6., num_pts)
