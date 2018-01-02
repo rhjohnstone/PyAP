2b6464d17c43f1e4859011012c7abcd52609a51d
Ross Johnstone <rhjohnstone@gmail.com>
2018-01-02 17:10:46 +0000

    Plot subset MCMCs

--- src/python/general_plot_histograms_lnG_subset.py
+++ src/python/general_plot_histograms_lnG_subset.py
@@ -218,12 +218,12 @@ while count < 2:
                 if j==num_params-1:
                     axes[str(i)+str(j)].set_xlabel("$"+labels[j]+"$")
                 else:
-                    axes[str(i)+str(j)].set_xlabel("log($"+labels[indices_to_keep[j]]+"$)")
+                    axes[str(i)+str(j)].set_xlabel("log($"+labels[j]+"$)")
             if j==0 and i>0:
                 if i==num_params_to_fit-1:
                     axes[str(i)+str(j)].set_ylabel("$"+labels[i]+"$")
                 else:
-                    axes[str(i)+str(j)].set_ylabel("log($"+labels[indices_to_keep[i]]+"$)")
+                    axes[str(i)+str(j)].set_ylabel("log($"+labels[i]+"$)")
                 
             plt.xticks(rotation=30)
     norm = matplotlib.colors.Normalize(vmin=colormin,vmax=colormax)
