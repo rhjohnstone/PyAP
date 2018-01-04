7aa46e72fbbfb186461ee9edf9095278ac1b8108
Ross Johnstone <rhjohnstone@gmail.com>
2018-01-04 16:35:52 +0000

    Gary predictive for Roche subsets

--- src/python/compute_and_save_gary_predictives.py
+++ src/python/compute_and_save_gary_predictives.py
@@ -130,12 +130,14 @@ for i in xrange(num_params_to_fit-1):
         else:
             xs[i, :] = np.linspace(mins[i]-4., maxs[i]+6., num_pts)
     elif expt_name=="roche_ten_tusscher_correct_units_subset":
-        if i==0 or i==1:
+        if i==0:
             xs[i, :] = np.linspace(mins[i]-0.1, maxs[i]+0.1, num_pts)
+        elif i==1:
+            xs[i, :] = np.linspace(mins[i]-0.3, maxs[i]+0.1, num_pts)
         elif i==2 or i==3:
             xs[i, :] = np.linspace(mins[i]-0.3, maxs[i]+0.3, num_pts)
         else:
-            xs[i, :] = np.linspace(mins[i]-0.5, maxs[i]+0.5, num_pts)
+            xs[i, :] = np.linspace(mins[i]-0.5, maxs[i]+0.8, num_pts)
 
 
 T = args.num_samples
