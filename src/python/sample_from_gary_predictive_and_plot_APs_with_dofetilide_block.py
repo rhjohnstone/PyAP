daf9a90b2006d91c4c94512915c1696784800c92
Ross Johnstone <rhjohnstone@gmail.com>
2018-01-09 18:09:33 +0000

    One block sample per channel per AP sample

--- src/python/sample_from_gary_predictive_and_plot_APs_with_dofetilide_block.py
+++ src/python/sample_from_gary_predictive_and_plot_APs_with_dofetilide_block.py
@@ -228,7 +228,7 @@ temp_Gs = np.copy(original_gs)
 for t in xrange(T):
     unif_samples = npr.rand(num_gs)
     temp_lnG_samples = [np.interp(unif_samples[p], gary_predictives[p][:,1], gary_predictives[p][:,0]) for p in xrange(num_gs)]
-    temp_G_samples = npexp(temp_lnGs)
+    temp_G_samples = npexp(temp_lnG_samples)
     temp_Gs[indices_to_keep] = temp_G_samples
     block_idx = npr.randint(0, block_length, num_channels)
     blocks = np.zeros(num_channels)
