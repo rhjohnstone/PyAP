98d1846d91ecfd3e80cfebff39375150b1e40e82
Ross Johnstone <rhjohnstone@gmail.com>
2018-02-02 20:54:04 +0900

    Simplified example

--- src/python/pyap_example.py
+++ src/python/pyap_example.py
@@ -8,9 +8,6 @@ import itertools as it
 import numpy.random as npr
 
 
-def example_likelihood_function(trace):
-    return np.sum(trace**2)
-    
 def sos(test_trace):
     return np.sum((expt_trace-test_trace)**2)
 
@@ -25,83 +22,40 @@ def sos(test_trace):
 # 8. Gokhale 2017 ex293 (not available atm)
 
 protocol = 1
-
+model_number = 3
+noise_sd = 0.5
 
 solve_start,solve_end,solve_timestep,stimulus_magnitude,stimulus_duration,stimulus_period,stimulus_start_time = ps.get_protocol_details(protocol)
-for model_number in xrange(3,4):
+solve_end = 400
+times = np.arange(solve_start,solve_end+solve_timestep,solve_timestep)
 
-    solve_end = 400
-    times = np.arange(solve_start,solve_end+solve_timestep,solve_timestep)
+original_gs, g_parameters, model_name = ps.get_original_params(model_number)
+original_gs = np.array(original_gs)
 
-    original_gs, g_parameters, model_name = ps.get_original_params(model_number)
-    original_gs = np.array(original_gs)
+# set up APSimulator --- there are more options available, see CMA-ES/MCMC codes
+ap = ap_simulator.APSimulator()
+ap.DefineStimulus(stimulus_magnitude,stimulus_duration,stimulus_period,stimulus_start_time)
+ap.DefineSolveTimes(solve_start,solve_end,solve_timestep)
+ap.DefineModel(model_number)
 
-    ap = ap_simulator.APSimulator()
-    ap.DefineStimulus(stimulus_magnitude,stimulus_duration,stimulus_period,stimulus_start_time)
-    ap.DefineSolveTimes(solve_start,solve_end,solve_timestep)
-    ap.DefineModel(model_number)
-    
-    ap.SetToModelInitialConditions()
-    
-    ICs = ap.GetStateVariables()
-    num_vars = len(ICs)
-    T = 20
-    all_state_vars = np.zeros((T+1, num_vars))
-    all_state_vars[0, :] = ICs
-    for t in xrange(T):
-        expt_trace = ap.SolveForVoltageTraceWithParams(original_gs)
-        all_state_vars[t+1, :] = ap.GetStateVariables()
-    
-    for j in xrange(num_vars):
-        fig, ax = plt.subplots(1, 1)
-        ax.plot(all_state_vars[:, j])
-    plt.show()
-    sys.exit()
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.grid()
-    ax.plot(times, expt_trace)
-    ax.set_title(model_name)
-    plt.show(block=True)
-    sys.exit()
-    
-    p_size = 0.05
-    perturbations = [p_size*np.ones(len(original_gs)), -p_size*np.ones(len(original_gs))]
-    sosses = []
-    how_many_perturbs = 10
-    while len(perturbations) < how_many_perturbs:
-        temp_p = p_size*(npr.randint(0,2,len(original_gs))*2.-1.)
-        if not any((temp_p == x).all() for x in perturbations):
-            perturbations.append(temp_p)
-    print perturbations
-    
-    
-    #ax.grid()
-    
-    for p in perturbations:
-        scaled_gs = (1.+p) * original_gs
-        try:
-            ap.SetToModelInitialConditions()
-            trace = ap.SolveForVoltageTraceWithParams(scaled_gs)
-        except ap_simulator.CPPException as e:
-            print e.GetMessage
-            sys.exit()
-        print "\n", (1.+p)
-        ss = sos(trace)
-        print ss
-        sosses.append(ss)
-        #ax.plot(times, trace, label=m)
-    #ax.set_ylabel("Membrane voltage (mV)")
-    #axs.set_xlabel("Time (ms)")
-    
-    #ax.set_title(model_name)
-    #ax.legend()
-    
-    print sosses
-    
-    print "Min sos: {} from {}\n".format(np.min(sosses), perturbations[np.argmin(sosses)])
-    
-    #fig.tight_layout()
-    #plt.show(block=True)
+
+
+#ICs = ap.GetStateVariables()
+ap.SetToModelInitialConditions()
+expt_trace = ap.SolveForVoltageTraceWithParams(original_gs) + noise_sd*npr.randn(len(times))
+
+# reset ICs before each solve, normally I put this inside of a newly-defined solve function so it always happens
+ap.SetToModelInitialConditions()
+test_trace = ap.SolveForVoltageTraceWithParams(1.5*original_gs)
+
+example_sos = sos(test_trace)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.grid()
+ax.plot(times, expt_trace, label='expt')
+ax.plot(times, test_trace, label='test')
+ax.set_title(model_name)
+ax.legend(loc="best")
+plt.show(block=True)
 
